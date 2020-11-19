684aec74b2b98cb93794fdbc3c5bc2dd06229631
Renad <ren9001@hotmail.com>
2020-11-20 00:04:45 +0300

    minor change

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { List, Task, User } = require('./db/models');
 
 const jwt = require('jsonwebtoken');
 // for heroku 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 /* MIDDLEWARE  */
 
 // Load middleware
