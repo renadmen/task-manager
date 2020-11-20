8bd17431238229189f3007585e806a3bc14fca82
Renad <ren9001@hotmail.com>
2020-11-20 03:43:21 +0300

    Deployment configuration

--- app.js
+++ app.js
@@ -396,10 +396,13 @@ app.get('/', (req, res) => {
     res.send('invaild endpoint');
   });
   
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-  });
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public/index.html'));
+//   });
 
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 app.listen(port, () => {
     console.log('Server is listening on port' + port);
