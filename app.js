4856fbd503972606dd1bfb31689f341aa613a86c
Renad <ren9001@hotmail.com>
2020-11-21 04:17:46 +0300

    .

--- app.js
+++ app.js
@@ -396,9 +396,9 @@ app.get('/', (req, res) => {
     res.send('invaild endpoint');
   });
   
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public/index.html'));
-//   });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
 
 // Create link to Angular build directory
 // var distDir = __dirname + "/dist/";
